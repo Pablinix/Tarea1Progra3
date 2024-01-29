144f2d309dc469e0ee5519b97651fe8d2d9fc35e
Pablinix <157521050+Pablinix@users.noreply.github.com>
2024-01-28 22:25:13 -0600

    Add files via upload

---
+++ Laboratorio2.py
@@ -0,0 +1,25 @@
+# Programa 1 
+print("¡Hola Mundo!")
+
+# Programa 2 
+saludo = "¡Hola Mundo!"
+print(saludo)
+
+# Programa 3
+nombre = input("Por favor, introduce tu nombre: ")
+saludo = f"¡Hola {nombre}!"
+print(saludo)
+
+# Programa 4
+resultado = (3 + 2 / 2 * 5) ** 2
+print("El resultado de la operación al cuadrado es:", resultado)
+
+# Programa 5
+horas_trabajo = float(input("escribe el número de horas trabajadas: "))
+costo = float(input("ingresa el costo por hora: "))
+
+pago = horas_trabajo * costo
+
+print(f"La paga correspondiente es: {pago}")
+
+
